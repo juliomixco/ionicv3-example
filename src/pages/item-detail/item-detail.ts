51a108d4b5cda22d02b77f8907c7073fc0cb5f3a
juliomixco <julio.mixg@gmail.com>
2017-06-20 19:01:36 -0600

    * query all films

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+
 
 import { Items } from '../../providers/providers';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/forkJoin';
 
 @Component({
   selector: 'page-item-detail',
@@ -10,8 +14,20 @@ import { Items } from '../../providers/providers';
 export class ItemDetailPage {
   item: any;
 
-  constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
+  constructor(public navCtrl: NavController, public http: Http, navParams: NavParams, items: Items) {
     this.item = navParams.get('item') || items.defaultItem;
+    this.getFilms().subscribe(results => {
+      this.item.filmInfo = results;
+    });
+  }
+  getFilms() {
+    var requestArray = []
+    for (var index = 0; index < this.item.films.length; index++) {
+      requestArray.push(this.http.get(this.item.films[index]).map(resp => resp.json()));
+
+    }
+    return Observable.forkJoin(requestArray);
+
   }
 
 }
